4f0e8c4e7329f8cfa65fb067ece1d6bd3e267d06
Miguel Pinzon <cmpinzonh@unal.edu.co>
2019-05-16 10:50:38 -0500

    Added spaces to ease readability

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -7,27 +7,30 @@ const githubUserName = 'srestrepoo';
 
 describe('Tests using the PUT method', () => {
   let userFollowing;
-
   let followUser;
 
   before(() => {
     followUser = agent.put(`${urlBase}/user/following/${githubUserName}`)
       .auth('token', process.env.ACCESS_TOKEN);
   });
+
   it('Check for response of PUT', () => followUser.then((response) => {
     expect(response.status).to.equal(statusCode.NO_CONTENT);
     expect(response.body).to.eql({});
   }));
+
   describe('Check users following list', () => {
     let userFollowingQuery;
     before(() => {
       userFollowingQuery = agent.get(`${urlBase}/user/following`)
         .auth('token', process.env.ACCESS_TOKEN);
     });
+
     it('user should have been followed', () => userFollowingQuery.then((response) => {
       userFollowing = response.body.find(users => users.login === githubUserName);
       expect(userFollowing.login).to.be.equal(githubUserName);
     }));
+
     describe('Check function indempotency', () => {
       let userFollowingAgain;
       let followUserAgain;
@@ -35,15 +38,18 @@ describe('Tests using the PUT method', () => {
         followUserAgain = agent.put(`${urlBase}/user/following/${githubUserName}`)
           .auth('token', process.env.ACCESS_TOKEN);
       });
+
       it('Check method response', () => followUserAgain.then((response) => {
         expect(response.status).to.equal(statusCode.NO_CONTENT);
         expect(response.body).to.eql({});
       }));
+
       describe('Re-check users following list', () => {
         before(() => {
           userFollowingAgain = agent.get(`${urlBase}/user/following`)
             .auth('token', process.env.ACCESS_TOKEN);
         });
+
         it('user should still be followed', () => userFollowingAgain.then((response) => {
           userFollowingAgain = response.body.find(user => user.login === githubUserName);
           expect(userFollowingAgain.login).to.be.equal(githubUserName);
