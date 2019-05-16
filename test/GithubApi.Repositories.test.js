9d218ca2ef55ee112ac0e09fc874577154cfb3e2
Miguel Pinzon <cmpinzonh@unal.edu.co>
2019-05-16 08:48:48 -0500

    Added tests with the GET and PUT methods, using Github's API

---
+++ test/GithubApi.Repositories.test.js
@@ -0,0 +1,37 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com/users/aperdomob';
+// const githubUserName = 'aperdomob';
+
+describe('Github Api Test', () => {
+  describe('Authentication', () => {
+    it('Test para Alejandro Perdomo', () => agent.get(`${urlBase}`)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.have.property('name');
+        expect(response.body.name).to.equal('Alejandro Perdomo');
+        expect(response.body.location).to.equal('Colombia');
+        expect(response.body.company).to.equal('PSL');
+      }));
+  });
+
+  describe('Test for specific repository', () => {
+    const expectedRepo = 'jasmine-awesome-report';
+    let repositories;
+    let repository;
+    before(() => {
+      const reposQuery = agent.get(`${urlBase}/repos`)
+        .then((response) => {
+          repositories = response.body;
+          repository = repositories.find(repos => repos.name === expectedRepo);
+        });
+      return reposQuery;
+    });
+    it('Check repo, privacity and description', () => {
+      expect(repository.full_name).to.equal(`aperdomob/${expectedRepo}`);
+      expect(repository.private).to.equal(false);
+    });
+  });
+});
