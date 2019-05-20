5672bfd2ceacfd11fa7adbd3d6326a67a8b2fb99
Miguel Pinzon Hernandez <cmpinzonh@unal.edu.co>
2019-05-20 10:08:28 -0500

    Changes to tests

--- test/GithubApi.Issue.test.js
+++ test/GithubApi.Issue.test.js
@@ -14,12 +14,14 @@ describe('Check if there is a logged in user', () => {
       });
     return userLogged;
   });
-  
+
   it('Check if the user has public repos', () => {
     expect(user.public_repos).to.be.above(0);
   });
+
   describe('Pick a repository in the array', () => {
     let repository;
+
     before(() => {
       const aRepository = agent.get(user.repos_url)
         .auth('token', process.env.ACCESS_TOKEN)
@@ -28,11 +30,14 @@ describe('Check if there is a logged in user', () => {
         });
       return aRepository;
     });
+
     it('The repository exists', () => {
       expect(repository).to.not.equal('undefined');
     });
+
     describe('Create an issue in the repo', () => {
       let issue;
+
       const title = { title: 'This is just a test' };
       before(() => {
         const issueCreate = agent.post(`${urlBase}/repos/${user.login}/${repository.name}/issues`, title)
@@ -45,12 +50,13 @@ describe('Check if there is a logged in user', () => {
 
       it('Check if the issues body is empty', () => {
         expect(issue.title).to.be.equal(title.title);
-        expect(issue.body).to.be.equal('{}');
+        expect(issue.body).to.be.equal(null);
       });
 
       describe('Edit the body of the issue', () => {
         const updateBodyIssue = { body: 'body updated', state: 'closed' };
         let updatedIssue;
+
         before(() => {
           const issueUpdate = agent.patch(`${urlBase}/repos/${user.login}/${repository.name}/issues/${issue.number}`, updateBodyIssue)
             .auth('token', process.env.ACCESS_TOKEN)
@@ -59,6 +65,7 @@ describe('Check if there is a logged in user', () => {
             });
           return issueUpdate;
         });
+
         it('Check that the new body was created correctly', () => {
           expect(updatedIssue.title).to.be.equal(title.title);
           expect(updatedIssue.body).to.be.equal(updateBodyIssue.body);
