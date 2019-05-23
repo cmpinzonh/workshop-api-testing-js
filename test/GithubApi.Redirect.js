a3c92ea52f19d63a11820b80b3a61fa1257b34d5
Miguel Pinzón <36303361+cmpinzonh@users.noreply.github.com>
2019-05-23 08:17:18 -0500

    Added tests usig HEAD method (#11)

---
+++ test/GithubApi.Redirect.js
@@ -0,0 +1,38 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const { expect } = require('chai');
+const statusCode = require('http-status-codes');
+
+const redirectURL = 'https://github.com/aperdomob/redirect-test';
+const newURL = 'https://github.com/aperdomob/new-redirect-test';
+let newURLResponseStatus;
+
+describe('Redirection test', () => {
+  let redirectQuery;
+
+  before(() => {
+    redirectQuery = agent.head(redirectURL)
+      .auth('token', process.env.ACCESS_TOKEN);
+  });
+
+  it('Check new URL is in error response', () => redirectQuery.catch((error) => {
+    expect(error.status).to.be.equal(301);
+    expect(error.response.headers.location).to.be.equal(newURL);
+  }));
+
+  describe('Test new URL', () => {
+    let newURLQuery;
+
+    before(() => {
+      newURLQuery = agent.get(newURL)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          newURLResponseStatus = response.status;
+        });
+      return newURLQuery;
+    });
+
+    it('Response status should be OK', () => {
+      expect(newURLResponseStatus).to.be.equal(statusCode.OK);
+    });
+  });
+});
